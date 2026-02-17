918a1fff266468bce4946ec042487af10ed7cbd5
Quack <ghalepaat@gmail.com>
2026-02-17 15:45:07 +0545

    Update test.py
    testing error

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ print("version 2")
 print("version 3 adding something to test poll scm build trigger")
 # now testing what is we have error in our code
 
-printf("is this working or Not ?" );
+sout("is this working or Not ?" );;;;;
