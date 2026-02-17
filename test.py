660f15502f240b69a4bce958ba56679fa4ea73b0
Quack <ghalepaat@gmail.com>
2026-02-17 16:00:18 +0545

    Update test.py
    removing the error

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ print("version 2")
 print("version 3 adding something to test poll scm build trigger")
 # now testing what is we have error in our code
 
-print { hellow wordld };
+# print { hellow wordld };
