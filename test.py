446999eeb00bac844b951d3e84f2e33e20f4cc77
Quack <ghalepaat@gmail.com>
2026-02-17 15:43:04 +0545

    Update test.py
    checking jenkins will show and error or not

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ print(" hello, learning  jenkins going well " )
 print("version 1")
 print("version 2")
 print("version 3 adding something to test poll scm build trigger")
+# now testing what is we have error in our code
+
+printf("is this working or Not ?" );
